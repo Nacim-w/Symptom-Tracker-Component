505bb71917c7c93d3b2ceb2ac37507111178f1b1
Nacim <nacim.msallem@gmail.com>
2024-04-21 00:12:08 +0100

    first

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const data = {
-    labels: ["2018", "2019", "2020", "2021", "2022", "2023", "2024"],
-    datasets: [
-      {
-        label: "Overall Progress",
-        data: [45, 50, 55, 60, 65, 70, 75],
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "rgba(255, 99, 132, 1)",
-        borderWidth: 1
-      }
-    ]
-  };
+const memes = ["2.jpg", "3.jpeg", "download.jpg"];
+function displayRandomMeme() {
+  const randomIndex = Math.floor(Math.random() * memes.length);
+  const randomMemeUrl = memes[randomIndex];
+  const memeImg = document.getElementById("memeImg");
+  memeImg.src = randomMemeUrl;
+}
+window.addEventListener("load", displayRandomMeme);
 
-  const ctx = document.getElementById('progressChart').getContext('2d');
-  const progressChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-});
