703d4cb73a224708eeaafc41b40c35ed58d303c2
Nacim <nacim.msallem@gmail.com>
2024-04-21 01:34:24 +0100

    first

--- script.js
+++
@@ -1,9 +0,0 @@
-const memes = ["2.jpg", "3.jpeg", "download.jpg"];
-function displayRandomMeme() {
-  const randomIndex = Math.floor(Math.random() * memes.length);
-  const randomMemeUrl = memes[randomIndex];
-  const memeImg = document.getElementById("memeImg");
-  memeImg.src = randomMemeUrl;
-}
-window.addEventListener("load", displayRandomMeme);
-
